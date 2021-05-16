403c323a07c8058f189c0259bfdd4bfb8ce68a77
Edward Horsey <edwardhorsey@protonmail.com>
2021-05-16 20:53:30 +0100

    Change to 2 space indentation

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send([
-        "<h2>Hi There Ed</h2>",
-        `<p>Secret: ${process.env.EDWARD}</p>`,
-        `<p>Using port: ${process.env.PORT} inside container</p>`,
-    ].join(''));
+  res.send([
+    "<h2>Hi There Ed</h2>",
+    `<p>Secret: ${process.env.EDWARD}</p>`,
+    `<p>Using port: ${process.env.PORT} inside container</p>`,
+    `<p>NODE_ENVIRONMENT: ${process.env.NODE_ENVIRONMENT}.</p>`,
+  ].join(''));
 });
 
 const port = process.env.PORT || 3000;
