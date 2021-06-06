ab7e890ca32b8d8c55da79ad8e9e7b98b01e0233
Edward <edwardhorsey@protonmail.com>
2021-06-06 17:56:52 +0100

    Add mongo address to env file. Import env through config.js file.

---
+++ config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    MONGO_IP: process.env.MONGO_IP || "mongo",
+    MONGO_PORT: process.env.MONGO_PORT || "27017",
+    MONGO_USER: process.env.MONGO_USER,
+    MONGO_PASSWORD: process.env.MONGO_PASSWORD,
+    PORT: process.env.PORT,
+    EDWARD: process.env.EDWARD,
+    NODE_ENVIRONMENT: process.env.NODE_ENVIRONMENT,
+}
--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT, EDWARD, PORT, NODE_ENVIRONMENT } = require("./config/config");
 
 const app = express();
 
-const mongoIpAddress = 'mongo';
-const mongoPort = '27017';
+const mongoAddress = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
 mongoose
-  .connect(`mongodb://ed:password@${mongoIpAddress}:${mongoPort}/?authSource=admin`)
+  .connect(mongoAddress)
   .then(() => console.log("successfully connected to DB"))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send([
     "<h2>Hi There Ed</h2>",
-    `<p>Secret: ${process.env.EDWARD}</p>`,
-    `<p>Using port: ${process.env.PORT} inside container</p>`,
-    `<p>NODE_ENVIRONMENT: ${process.env.NODE_ENVIRONMENT}.</p>`,
+    `<p>Secret: ${EDWARD}</p>`,
+    `<p>Using port: ${PORT} inside container</p>`,
+    `<p>NODE_ENVIRONMENT: ${NODE_ENVIRONMENT}.</p>`,
   ].join(''));
 });
 
-const port = process.env.PORT || 3000;
+const port = PORT || 3000;
 
 app.listen(port, () => console.log(`listening on port ${port}`));
