cb5795497773508157a5c6bf098e438a4d27730d
Edward <edwardhorsey@protonmail.com>
2021-06-11 19:36:10 +0100

    add login endpoint

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,9 +3,8 @@ const User = require('../models/userModel');
 
 exports.signUp = async (req, res) => {
   const { username, password } = req.body;
-  const hashpassword = await bcrypt.hash(password, 12);
-
   try {
+    const hashpassword = await bcrypt.hash(password, 12);
     const newUser = await User.create({
       username,
       password: hashpassword,
@@ -19,6 +18,35 @@ exports.signUp = async (req, res) => {
     });
   } catch (e) {
     res.status(400).json({
+      status: 'fail',
+    });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(404).json({
+        status: 'fail',
+        message: 'user not found',
+      });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (passwordMatch) {
+      return res.status(200).json({
+        status: 'success',
+      });
+    }
+
+    return res.status(400).json({
+      status: 'fail',
+      message: 'incorrect username or password',
+    });
+  } catch (e) {
+    return res.status(400).json({
       status: 'fail',
     });
   }
