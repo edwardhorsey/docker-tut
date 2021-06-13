1eee2972b999c49ca8847ec871a319f727d06f93
Edward <edwardhorsey@protonmail.com>
2021-06-13 17:31:26 +0100

    Add express-session, redis and connect-redis packages

--- config/config.js
+++ config/config.js
@@ -1,9 +1,12 @@
 module.exports = {
   MONGO_IP: process.env.MONGO_IP || 'mongo',
-  MONGO_PORT: process.env.MONGO_PORT || '27017',
+  MONGO_PORT: process.env.MONGO_PORT || 27017,
   MONGO_USER: process.env.MONGO_USER,
   MONGO_PASSWORD: process.env.MONGO_PASSWORD,
   PORT: process.env.PORT,
   EDWARD: process.env.EDWARD,
   NODE_ENVIRONMENT: process.env.NODE_ENVIRONMENT,
+  REDIS_URL: process.env.REDIS_URL || 'redis',
+  REDIS_PORT: process.env.REDIS_PORT || 6379,
+  SESSION_SECRET: process.env.SESSION_SECRET,
 };
